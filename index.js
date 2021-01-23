b1c6f1ccf3513529bd94bfa5e8e187fcc4fa78ac
Alex Alexis <aalexis8@gmail.com>
2021-01-23 09:57:26 -0500

    connection string, and mongoose connection added

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://prox6venode01.leahbelle.lan:2717/helpCamp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+// .then(() => {
+//   console.log("MONGODB IS AVAILABLE!!");
+// })
+// .catch((err) => {
+//   console.log("DIDN'T CONNECT TO MONGODB!!!");
+//   console.log(err);
+// });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
