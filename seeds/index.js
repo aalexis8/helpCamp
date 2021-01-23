92f9283672921851a0e27f4a0aa4ebb5ba4043f8
Alex Alexis <aalexis8@gmail.com>
2021-01-23 11:45:09 -0500

    db seeding scaffolding, cities, index helpers

---
+++ seeds/index.js
@@ -0,0 +1,16 @@
+// The seed file will be self contained
+// It connect to db on it's on
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://prox6venode01.leahbelle.lan:2717/helpCamp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
