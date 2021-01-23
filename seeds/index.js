f85c39d46b325c85d411b399c9392318dcdd9861
Alex Alexis <aalexis8@gmail.com>
2021-01-23 14:26:39 -0500

    seeding camps with random title and location

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,8 @@
 // It connect to db on it's on
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+const cities = require("./cities");
 
 mongoose.connect("mongodb://prox6venode01.leahbelle.lan:2717/helpCamp", {
   useNewUrlParser: true,
@@ -15,10 +17,22 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+// get random element from array
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
-  const c = new Campground({ title: "strawberry field" });
-  await c.save();
+  // const c = new Campground({ title: "strawberry field" });
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+  // await c.save();
 };
 
-seedDB();
+seedDB().then(() => {
+  mongoose.connection.close();
+});
