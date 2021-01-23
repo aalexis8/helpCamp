eac7b2d2bb46f02a86fa1d762ace6759513d0366
Alex Alexis <aalexis8@gmail.com>
2021-01-23 11:51:11 -0500

    testing connection of seed file, deletion and insertion, success.

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 // The seed file will be self contained
 // It connect to db on it's on
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://prox6venode01.leahbelle.lan:2717/helpCamp", {
   useNewUrlParser: true,
@@ -14,3 +14,11 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  const c = new Campground({ title: "strawberry field" });
+  await c.save();
+};
+
+seedDB();
